9040241a8d22f75166bc19d81572274e1d10be68
Fernando Aramburu Merlos <faramburumerlos@gmail.com>
2021-12-22 11:49:24 -0800

    add tropic lines

--- 98_MapCroplandArea_lat_lon.R
+++ 98_MapCroplandArea_lat_lon.R
@@ -15,24 +15,89 @@ if(system('hostname', TRUE) == "ESP-RH-9891"){
 # country borders
 countries <- geodata::world(resolution = 3, path = "InData/countries")
 
+
+# tropics
+w <- countries
+# to use a faster buffering method (not lonlat)
+crs(w) <- "+proj=utm +zone=1"
+b <- buffer(w, 2)
+trop <- vect(c("LINESTRING(-180 23, 180 23)", "LINESTRING(-180 -23, 180 -23)"))
+trop <- erase(trop, b)
+equ <- vect(c("LINESTRING(-180 0, 180 0)", "LINESTRING(-180 0, 180 0)"))
+equ <- erase(equ, b)
+
+
 # total cropland (ha) per cell
 totcl <- rast("InData/TotalCropland.tif")
 ar = ncol(totcl)/nrow(totcl)
 
 
-
-# cropland by lat and lon
-dlon <- fread("OutData/DfxLon.csv")
+# cropland by lat 
 dlat <- fread("OutData/DfxLat.csv")
-
-dlon[, Mha:= tot.area/1e6]
 dlat[, Mha:= tot.area/1e6]
 
 
+# plot lat -------------
+
+tiff(filename = paste0('G:/My Drive/globcropdiv/Maps/Total_Cropland_lat.tif'), 
+     units = "in",
+     width = ncol(totcl)/600 * 1.2, 
+     height = (ncol(totcl)/600)/ar, 
+     type = "cairo", res = 300, 
+     compression = "zip")
+
+layout(mat = matrix(c(1,2), ncol = 2), heights = 1, widths = c(5,2))
+
+
+# map
+plot(totcl, type = "continuous", mar = c(2, 4, 2, 5),
+     pax = list(lwd = 0, labels = FALSE),
+     col = mako(256, direction = -1),
+     maxcell = ncell(totcl))
+plot(countries, lwd = 0.4, add = T)
+plot(trop, col = "grey50", lty = 3, lwd = 0.2, add = T)
+plot(equ, col = "grey50", lwd = 0.2, add = T)
+mtext("a", line = -2, adj = 0.05, font = 2, cex = 1.5)
+
+
+# lat
+lat_tcl <- loess(Mha ~ lat, data = dlat, span = 0.1)
+
+par(mar = c(3.5,2.5,2.8,1), xpd = NA)
+plot(dlat$lat ~ dlat$Mha, 
+     pch = 22, bg = "dark green", cex = 0.5, 
+     xlab = "",
+     ylab = "", 
+     axes = F,
+     xlim = c(0, 25),
+     ylim = c(-82,95))
+lines(dlat$lat ~ predict(lat_tcl),  col = "dark green", lwd = 3)
+axis(side = 1, at = seq(0,25,5), cex.axis = 0.9)
+axis(side = 2, at = seq(-90, 90, 30), cex.axis = 0.9)
+mtext("Area (Mha)", side = 1, line = 2.2)
+mtext("latidude", side = 2, line = 2.2)
+mtext("b", line = -1.3, adj = 0.05, font = 2, cex = 1.5)
+clip(-0.5, 25.05, -90, 90)
+abline(h = c(-23, 23), lty = 3, lwd = 0.4)
+abline(h = 0, lwd = 0.4)
+
+dev.off()
 
 
 
-# plot -------------
+
+
+
+
+
+
+
+
+
+
+
+
+# plot lat lon -------------
 
 tiff(filename = paste0('G:/My Drive/globcropdiv/Maps/Total_Cropland.tif'), 
      units = "in",
--- 99_Mapping.R
+++ 99_Mapping.R
@@ -10,7 +10,20 @@ if(system('hostname', TRUE) == "ESP-RH-9891"){
 } # else if { ... 
 
 # country borders
-countries <- geodata::world(resolution = 1, path = "InData/countries")
+countries <- geodata::world(resolution = 3, path = "InData/countries")
+
+# tropics
+w <- countries
+# to use a faster buffering method (not lonlat)
+crs(w) <- "+proj=utm +zone=1"
+b <- buffer(w, 2)
+trop <- vect(c("LINESTRING(-180 23, 180 23)", "LINESTRING(-180 -23, 180 -23)"))
+trop <- erase(trop, b)
+equ <- vect(c("LINESTRING(-180 0, 180 0)", "LINESTRING(-180 0, 180 0)"))
+equ <- erase(equ, b)
+
+
+
 # total cropland (ha) per cell
 totcl <- rast("InData/TotalCropland.tif")
 
@@ -41,11 +54,12 @@ tiff(filename = paste0('G:/My Drive/globcropdiv/Maps/Total_Cropland.tif'),
      height = (ncol(totcl)/300)/ar, 
      type = "cairo", res = 300, 
      compression = "zip")
-par(mai = c(0,0,0,0), omi = c(0,0,0,0))
 plot(totcl, type = "continuous", axes = FALSE,
      col = mako(256, direction = -1),
      maxcell = ncell(totcl))
 plot(countries, lwd = 0.4, add = T)
+plot(trop, col = "grey50", lty = 3, lwd = 0.5, add = T)
+plot(equ, col = "grey50", lwd = 0.4, add = T)
 dev.off()
 
 
@@ -67,32 +81,32 @@ tiff(filename = 'G:/My Drive/globcropdiv/Maps/act_D.tif', units = "in",
      height = (ncol(act_D)/300)/ar, 
      type = "cairo", res = 300, 
      compression = "zip")
-plot(act_D, axes = FALSE, 
+plot(act_D, pax = list(lwd = 0, labels = FALSE), 
      type = "interval",
      col = mycolors,
      breaks = brks,
      #type = "continuous",
      #col = turbo(256, direction = -1),
      maxcell = ncell(act_D), 
-     mar = c(0,0,0,0),
+     mar = c(1,ar,1,ar),
      plg = list(legend = c("1 - 1.5", "1.5 - 2.5", "2.5 - 5", "5 - 10", "> 10"),
                 cex = 1.8, x = "bottomleft", 
                 title = expression(italic(cD)),
                 title.adj = 0.5))
 plot(countries, lwd = 0.4, add = T)
+plot(trop, col = "grey50", lty = 3, lwd = 0.5, add = T)
+plot(equ, col = "grey50", lwd = 0.4, add = T)
 dev.off()
 
 
 ## Potential #################################################
-
-
 pot_D_eco <- rast("OutData/pot_D_eco.tif")
 pot_D_eco <- crop(pot_D_eco, ex)
 pot_D_eco <- mask(pot_D_eco, mask = cl_mask, maskvalues = 1)
 
-
 pot_D_sdm <- rast("OutData/pot_D_sdm.tif")
 pot_D_sdm <- crop(pot_D_sdm, ex)
+pot_D_sdm <- mask(pot_D_sdm, mask = cl_mask, maskvalues = 1)
 
 
 tiff(filename = 'G:/My Drive/globcropdiv/Maps/pot_D.tif', units = "in",
@@ -102,73 +116,42 @@ tiff(filename = 'G:/My Drive/globcropdiv/Maps/pot_D.tif', units = "in",
      compression = "zip")
 par(mfrow = c(2,1))
 
-### Ecocrop -------
 
 brks <- seq(0, 175, length.out = 8)
 mycols <- turbo(length(brks)-1, direction = -1, begin = 0.1, end = 0.9)
 
-plot(pot_D_eco, axes = FALSE, 
+plot(pot_D_eco, pax = list(lwd = 0, labels = FALSE), 
      type = "interval",
      col = mycols,
      breaks = brks,
      maxcell = ncell(pot_D_eco), 
-     mar = c(0,1,2,1),
+     mar = c(1,ar,1,ar),
      plg = list(cex = 1.8, x = "bottomleft", 
                 title = expression("as-"*italic(pD)),
                 title.adj = 0.3))
 plot(countries, lwd = 0.4, add = T)
-mtext("a", line = .1, adj = 0.95, cex = 2, font = 2)
-
-### SDM -------
+plot(trop, col = "grey50", lty = 3, lwd = 0.5, add = T)
+plot(equ, col = "grey50", lwd = 0.4, add = T)
+mtext("a", line = -3, adj = 0.99, cex = 3, font = 2)
 
 brks <- seq(50, 125, length.out = 7)
 mycols <- turbo(length(brks)-1, direction = -1, begin = 0.1, end = 0.9)
 
-plot(pot_D_sdm, axes = FALSE, 
+plot(pot_D_sdm, pax = list(lwd = 0, labels = FALSE), 
      type = "interval",
      col = mycols,
      breaks = brks,
      maxcell = ncell(pot_D_sdm), 
-     mar = c(0,1,2,1),
+     mar = c(1,ar,1,ar),
      plg = list(cex = 1.8,  x = "bottomleft", 
                 title = expression("rs-"*italic(pD)),
                 title.adj = 0.3))
 plot(countries, lwd = 0.4, add = T)
-mtext("b", line = .1, adj = 0.95, cex = 2, font = 2)
+plot(trop, col = "grey50", lty = 3, lwd = 0.5, add = T)
+plot(equ, col = "grey50", lwd = 0.4, add = T)
+mtext("b", line = -3, adj = 0.99, cex = 3, font = 2)
 
 dev.off()
-
-
-
-
-
-
-
-
-#### Species level ---------------
-pot_D_eco_sp <- rast("OutData/pot_D_eco_sp.tif")
-pot_D_eco_sp <- crop(pot_D_eco_sp, ex)
-pot_D_eco_sp <- mask(pot_D_eco_sp, mask = cl_mask, maskvalues = 1)
-
-
-brks <- c(brks, 200, 250, 350)
-mycols <- turbo(length(brks)-1, direction = -1, begin = 0.1, end = 1)
-
-tiff(filename = 'G:/My Drive/globcropdiv/Maps/pot_D_eco_sp.tif', units = "in",
-     width = ncol(pot_D_eco_sp)/300, 
-     height = (ncol(pot_D_eco_sp)/300)/ar, 
-     type = "cairo", res = 300, 
-     compression = "zip")
-plot(pot_D_eco_sp, axes = FALSE, 
-     type = "interval",
-     col = turbo(length(brks)-1, direction = -1, begin = 0.1, end = 0.9),
-     breaks = brks,
-     maxcell = ncell(pot_D_eco_sp), 
-     mar = c(0,0,0,0),
-     plg = list(cex = 1.8, x = "left"))
-plot(countries, lwd = 0.4, add = T)
-dev.off()
-
 
 
 
@@ -178,10 +161,15 @@ dev.off()
 ## Attainable #################################################
 brks <- c(1,5,10,15,20,25,30,50)
 
-### SDM  -------------
 att_D_sdm <- rast("OutData/att_D_sdm.tif")
 att_D_sdm <- crop(att_D_sdm, ex)
 att_D_sdm <- mask(att_D_sdm, cl_mask, maskvalues = 1)
+
+att_D_eco <- rast("OutData/att_D_eco.tif")
+att_D_eco <- crop(att_D_eco, ex)
+att_D_eco <- mask(att_D_eco, cl_mask, maskvalues = 1)
+
+
 mycols <- turbo(length(brks)-1, direction = -1, begin = 0.1, end = 1)
 
 
@@ -193,79 +181,41 @@ tiff(filename = 'G:/My Drive/globcropdiv/Maps/att_D.tif', units = "in",
 par(mfrow = c(2,1))
 
 plot(att_D_sdm, 
-     axes = FALSE, 
+     pax = list(lwd = 0, labels = FALSE), 
      type = "interval", 
      col = mycols, 
      breaks = brks,
      maxcell = ncell(att_D_sdm), 
-     mar = c(0,1,2,1),
+     mar = c(1,ar,1,ar),
      plg = list(legend = c("1 - 5", "5 - 10", "10 - 15", "15 - 20", 
                            "20 - 25", "25 - 30","> 30"),
                 cex = 1.8, x = "bottomleft", 
                 title = expression("rs-"*italic(aD)),
                 title.adj = 0.3))
 plot(countries, lwd = 0.4, add = T)
-mtext("a", line = .2, cex = 2, font = 2, adj = 0.95)
-
-
-### Ecocrop -------------
-att_D_eco <- rast("OutData/att_D_eco.tif")
-att_D_eco <- crop(att_D_eco, ex)
-att_D_eco <- mask(att_D_eco, cl_mask, maskvalues = 1)
+plot(trop, col = "grey50", lty = 3, lwd = 0.5, add = T)
+plot(equ, col = "grey50", lwd = 0.4, add = T)
+mtext("a", line = -3, cex = 3, font = 2, adj = 0.99)
 
 
 plot(att_D_eco, 
-     axes = FALSE, 
+     pax = list(lwd = 0, labels = FALSE), 
      type = "interval", 
      col = mycols, 
      breaks = brks,
      maxcell = ncell(att_D_eco), 
-     mar = c(0,1,2,1),
+     mar = c(1,ar,1,ar),
      plg = list(legend = c("1 - 5", "5 - 10", "10 - 15", "15 - 20", 
                            "20 - 25", "25 - 30","> 30"),
                 cex = 1.8,  x = "bottomleft", 
                 title = expression("as-"*italic(aD)),
                 title.adj = 0.3))
 plot(countries, lwd = 0.4, add = T)
-mtext("b", line = .2, cex = 2, font = 2, adj = 0.95)
+plot(trop, col = "grey50", lty = 3, lwd = 0.5, add = T)
+plot(equ, col = "grey50", lwd = 0.4, add = T)
+mtext("b", line = -3, cex = 3, font = 2, adj = 0.99)
 dev.off()
 
-
-
-
-
-
-
-
-
-
-### Average -------------------------------
-brks <- seq(0, 40, length.out = 6)
-att_D_avg <- (att_D_eco + att_D_sdm)/2
-
-att_D_avg <- crop(att_D_avg, ex)
-att_D_avg <- mask(att_D_avg, cl_mask, maskvalues = 1)
-
-tiff(filename = 'G:/My Drive/globcropdiv/Maps/att_D_avg.tif', units = "in",
-     width = ncol(att_D_avg)/300, 
-     height = (ncol(att_D_avg)/300)/ar, 
-     type = "cairo", res = 300, 
-     compression = "zip")
-par(mai = c(0,0,0,0), omi = c(0,0,0,0))
-plot(att_D_avg, 
-     axes = FALSE, 
-     type = "interval", 
-     col = mycols, 
-     breaks = brks,
-     maxcell = ncell(att_D_avg), 
-     mar = c(0,0,0,0),
-     plg = list(legend = c("1 - 5", "5 - 10", "10 - 15", "15 - 20", 
-                           "20 - 25", "25 - 30","> 30"),
-                cex = 1.8,  x = "bottomleft", 
-                title = expression(paste(phantom(0)^a,italic(D))),
-                title.adj = 0.3))
-plot(countries, lwd = 0.4, add = T)
-dev.off()
 
 
 
@@ -274,55 +224,11 @@ dev.off()
 
 ## GAP ##################################################
 
-brks <- c(-Inf, 30, 45, 60, 75, 90, 100)
-mycols <- turbo(length(brks), begin = 0.1, end = 1)[-2]
-
-
-
-### Ecocrop ---------
-Dgap_eco <- (att_D_eco - act_D)/(att_D_eco) * 100
-
-tiff(filename = 'G:/My Drive/globcropdiv/Maps/Dgap_eco.tif', units = "in",
-     width = ncol(Dgap_eco)/300, 
-     height = (ncol(Dgap_eco)/300)/ar, 
-     type = "cairo", res = 300, 
-     compression = "zip")
-par(mai = c(0,0,0,0), omi = c(0,0,0,0))
-plot(Dgap_eco, 
-     axes = FALSE, 
-     type = "interval", 
-     col = mycols, 
-     breaks = brks,
-     maxcell = ncell(Dgap_eco), 
-     mar = c(0,0,0,0),
-     plg = list(cex = 1.8, x = "left"))
-plot(countries, lwd = 0.45, add = T)
-dev.off()
-
-### SDM -------------
-Dgap_sdm <-  (att_D_sdm - act_D)/(att_D_sdm) * 100
-
-
-tiff(filename = 'G:/My Drive/globcropdiv/Maps/Dgap_sdm.tif', units = "in",
-     width = ncol(Dgap_sdm)/300, 
-     height = (ncol(Dgap_sdm)/300)/ar, 
-     type = "cairo", res = 300, 
-     compression = "zip")
-par(mai = c(0,0,0,0), omi = c(0,0,0,0))
-plot(Dgap_sdm, 
-     axes = FALSE, 
-     type = "interval", 
-     col = mycols, 
-     breaks = brks,
-     maxcell = ncell(Dgap_sdm), 
-     mar = c(0,0,0,0),
-     plg = list(cex = 1.8, x = "left"))
-plot(countries, lwd = 0.45, add = T)
-dev.off()
-
-
+brks <- c(-Inf, 25, 50, 75, 100)
+mycols <- turbo(length(brks)-1, begin = 0.1, end = .9)
 
 ### avg -------------
+att_D_avg <- (att_D_eco + att_D_sdm)/2
 Dgap_avg <- (att_D_avg - act_D)/(att_D_avg) * 100
 
 
@@ -331,20 +237,21 @@ tiff(filename = 'G:/My Drive/globcropdiv/Maps/Dgap_avg.tif', units = "in",
      height = (ncol(Dgap_avg)/300)/ar, 
      type = "cairo", res = 300, 
      compression = "zip")
-par(mai = c(0,0,0,0), omi = c(0,0,0,0))
+
 plot(Dgap_avg, 
-     axes = FALSE, 
+     pax = list(lwd = 0, labels = FALSE), 
      type = "interval", 
      col = mycols,
      breaks = brks,
      maxcell = ncell(Dgap_avg), 
-     mar = c(0,0,0,0),
+     mar = c(1,ar,1,ar),
      plg = list(cex = 1.8, x = "bottomleft", 
                 title = expression(italic(D)*g ~ "(%)"),
                 title.adj = 0.3,
-                legend = c("< 30", "30 - 45", "45 - 60", "60 - 75",
-                           "75 - 90", "90 - 100")))
+                legend = c("< 25", "25 - 50", "50 - 75", "75 - 100")))
 plot(countries, lwd = 0.45, add = T)
+plot(trop, col = "grey50", lty = 3, lwd = 0.5, add = T)
+plot(equ, col = "grey50", lwd = 0.4, add = T)
 dev.off()
 
 # DQI ###################################################
@@ -362,12 +269,14 @@ tiff(filename = file.path("G:/My Drive/globcropdiv/Maps/DQI_apple.tif"),
      height = (ncol(r)/300)/ar, 
      type = "cairo", res = 300, 
      compression = "zip")
-par(mai = c(0,0,0,0), omi = c(0,0,0,0))
-plot(apple_dqi, type = "continuous", axes = FALSE,
+par(mar = c(1,ar,1,ar), omi = c(0,0,0,0))
+plot(apple_dqi, type = "continuous", pax = list(lwd = 0, labels = FALSE),
      col = cividis(256, direction = -1),
      maxcell = ncell(r), mar = c(2,0,2,6), 
      plg = list(cex = 1.5))
 plot(countries, lwd = 0.4, add = T)
+plot(trop, col = "grey50", lty = 3, lwd = 0.5, add = T)
+plot(equ, col = "grey50", lwd = 0.4, add = T)
 mtext("a", line = .2, adj = .95, font = 2, cex = 2)
 dev.off()
 
@@ -385,11 +294,13 @@ tiff(filename = file.path("G:/My Drive/globcropdiv/Maps/DQI_mango.tif"),
      type = "cairo", res = 300, 
      compression = "zip")
 par(mai = c(0,0,0,0), omi = c(0,0,0,0))
-plot(mango_dqi, type = "continuous", axes = FALSE,
+plot(mango_dqi, type = "continuous", pax = list(lwd = 0, labels = FALSE),
      col = cividis(256, direction = -1),
      maxcell = ncell(r), mar = c(2,0,2,6), 
      plg = list(cex = 1.5))
 plot(countries, lwd = 0.4, add = T)
+plot(trop, col = "grey50", lty = 3, lwd = 0.5, add = T)
+plot(equ, col = "grey50", lwd = 0.4, add = T)
 mtext("b", line = .2, adj = .95, font = 2, cex = 2)
 dev.off()
 
